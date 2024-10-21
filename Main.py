d6247f480bce937d0f71bd5c9a04306dfe4d6701
Javi3166 <javi3166@yahoo.com>
2024-10-20 21:52:10 -0400

    Added functions and math module import

---
+++ Main.py
@@ -0,0 +1,30 @@
+from math import *
+
+print(-2.0987)
+
+print(3 + 4.5)
+
+print(3 * (4 + 5))
+
+print(10 % 3)
+
+my_num = 5
+print(my_num)
+print(str(my_num) + " my favorite number.")
+
+my_num = -8
+print(abs(my_num))
+
+print(pow(3, 2))
+
+print(max(4, 6))
+
+print(min(4, 6))
+
+print(round(3.7))
+
+print(floor(3.7))
+
+print(ceil(3.1))
+
+print(sqrt(6))
